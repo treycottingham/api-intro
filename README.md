c509b303b6ace7350154b5003b64c5872c4656d3
Matt Winzer <matt.winzer@gMWad5ddf.local>
2018-03-19 11:10:36 -0600

    intial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# API Intro
+
+## Objectives
+
+- Explain what an API is
+- Use fetch to GET data from an external API
+- Append data from a fetch request to the DOM
+
+- Stretch: Identify API's you would like to use!
+
+## Notes
+
+## Resources
+
+- [Public APIs](https://github.com/toddmotto/public-apis#photography)
---
+++ app.js
@@ -0,0 +1,14 @@
+const baseUrl = 'https://swapi.co/api/'
+const characterSection = document.querySelector('.characters')
+
+// Write a function that GETs information about Luke Skywalker and add his name to the page
+getLuke()
+
+
+// Modify the getLuke function to also add:
+  // Luke's hair color
+  // Luke's height
+
+function getLuke() {
+
+}
